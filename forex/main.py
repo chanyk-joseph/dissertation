f28a4e99211f10f9a3d2e0697d36d7c2814289ea
Joseph Chan <chanyk.joseph@gmail.com>
2018-12-30 00:32:29 +0800

    Update

--- forex/main.py
+++ forex/main.py
@@ -13,8 +13,7 @@ def plot(df):
     df.plot(x='datetime', y='total_asset_value')
     plt.show()
 
-if __name__ == "__main__":
-    print(parseDateTime('20080103 23:01:01:100'))
+print(parseDateTime('20080103 23:01:01:100'))
 
 # script_dir = path.dirname(path.realpath(sys.argv[0]))
 # csvFile = path.join(script_dir, 'trade-record-simple.csv')
