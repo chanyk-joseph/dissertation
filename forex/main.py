2720b8d7dd586205ee417d5b642cc8285f49997a
Joseph Chan <chanyk.joseph@gmail.com>
2019-01-03 00:09:40 +0800

    Update

--- forex/main.py
+++ forex/main.py
@@ -2,12 +2,10 @@ import os.path as path
 import sys
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
 
-import talib
 from forex.utils import *
-
+from strategies.sma import SMA
 
 def plot(df):
     df.plot(x='datetime', y='total_asset_value')
@@ -23,17 +21,12 @@ data_dir = "F:\\modified_data"
 tradeRecordCSV = path.join(script_dir, 'trade-record-simple.csv')
 ohlcCSV = path.join(data_dir, 'EURUSD_Daily_(1-1-2008_31-12-2017).csv')
 
-df = pd.read_csv(ohlcCSV)
-df = set_df_Timestamp_as_datetime(df)
-print(get_df_by_datetime_range(df, parseISODateTime('2008-01-02T00:00:00'), parseISODateTime('2017-01-03T00:00:00')))
-
-
-
-
-# import talib
+df = set_df_Timestamp_as_datetime(pd.read_csv(ohlcCSV))
+# print(get_df_by_datetime_range(df, parseISODateTime('2008-01-02T00:00:00'), parseISODateTime('2017-01-03T00:00:00')))
 # close = np.random.random(100)
-# SMA = talib.MA(close,30,matype=0)
-
-# plt.plot(SMA)
+smaStrategy = SMA(60*60*24*3)
+result = smaStrategy.calculate(df, parseISODateTime('2001-01-07T00:00:00'))
+print(result)
+# plt.plot(result)
 # plt.ylabel('some numbers')
 # plt.show()
